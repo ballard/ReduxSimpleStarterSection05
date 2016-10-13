fdf84a6a9ba4bc3a968d6fe3b8f992341ac45d0e
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-13 11:23:50 +0300

    part 62 complete

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,13 +3,35 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component {
 
     renderWeather(cityData) {
+
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        //console.log(temps)
+
         return(
-            <tr key={cityData.city.id}>
+            <tr key={cityData.city.id}> // maybe name
                 <td >{cityData.city.name}</td>
+                <td>
+                    <Sparklines height={100} width={180} data={temps}>
+                        <SparklinesLine color="red"/>
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines height={100} width={180} data={pressures}>
+                        <SparklinesLine color="yellow"/>
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines height={100} width={180} data={humidities}>
+                        <SparklinesLine color="blue"/>
+                    </Sparklines>
+                </td>
             </tr>
         );
     };
