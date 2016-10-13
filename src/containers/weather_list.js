e6ed5af6c5064c1fa70a7699778f71c9b88d559c
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-13 10:30:48 +0300

    part 60 complete

---
+++ src/containers/weather_list.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ivanlazarev on 13.10.16.
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+    };
+}
+
+//
+// function mapStateToProps(state) {
+//     return { weather: state.weather };
+// }
+// weather infers from state and statement
+// equals to:
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
