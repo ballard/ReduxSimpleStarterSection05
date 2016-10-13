cc702afbd6b02b89c28f1c2b96a4bd7798ef8f88
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-13 09:53:19 +0300

    part 59 complete

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,11 @@
 /**
  * Created by ivanlazarev on 12.10.16.
  */
-export default function (state = null, action) {
+export default function (state = [], action) {
+    switch (action.type){
+        case 'FETCH_WEATHER':
+            return [ action.payload.data, ...state]; // OR state.concat(action.payload.data) i.e. creating new array and returning it
+    }
+
     return state;
 }
